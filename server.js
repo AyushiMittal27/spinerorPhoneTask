3470563b7ef3efd6e2150cb36826ab92346925d0
Ayushi Mittal <ayshiimtll@gmail.com>
2021-05-04 21:51:16 +0530

    added middleware , helpers and routes

--- server.js
+++ server.js
@@ -5,13 +5,47 @@ const dotenv = require("dotenv");
 const app = express();
 
 
-dotenv.path({ "./config.config.env"});
+dotenv.config({ path: "./config.config.env" });
 
 //middleware 
 app.use(cors());
 app.use(express.json());
 
 
+//connect to the database
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_LOCAL_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then((con) => {
+      console.log(
+        `MongoDb databse connected with host: ${con.connection.host}`
+      );
+    });
+};
+connectDatabase();
+
 app.listen(process.env.PORT, () => {
-  console.log('Server started on port 8000');
+  console.log(`Server started on port 8000 ${process.env.PORT}`);
 })
+
+//handling unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("shutting down the server due to Unhandled promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+//handling uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR : ${err.message}`);
+  console.log("Shutting down the server due to uncaught exception");
+  process.exit(1);
+});
+
